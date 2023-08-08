25be824dfb11a10101fe0e2651a13f5771adcc73
omar-albakri <albakrio@outlook.com>
2023-08-08 14:38:35 -0700

    add sliding window longest

--- src/algorithms/slidingWindow/slidingWindow.ts
+++ src/algorithms/slidingWindow/slidingWindow.ts
@@ -48,3 +48,22 @@ function findAllAnagrams(original: string, check: string): number[] {
 
 const result: number[] = findAllAnagrams('cbaebabacd', 'abc');
 console.log(result);
+
+function subarraySumLongest(nums: number[], target: number): number {
+	let windowSum = 0;
+	let length = 0;
+	let left = 0;
+
+	for (let right = 0; right < nums.length; ++right) {
+		windowSum = windowSum + nums[right];
+
+		while (windowSum > target) {
+			windowSum = windowSum - nums[left];
+			++left;
+		}
+
+		length = Math.max(length, right - left + 1);
+	}
+
+	return length;
+}
